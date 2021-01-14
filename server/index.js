57f90291dff85391fc24f41d0ecdaed9fe370a61
luiscarbajal <luisf144@hotmail.com>
2021-01-14 20:18:37 +0000

    edit sync models in index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ global.__basedir = __dirname;
 //Database connection
 //{ force: true } to execute the queries.
 db.sequelize.sync(
-    // { force: true }
+     { force: true }
 ).then(() => { console.log("Connected to DB") }).catch((err) => {console.log(err)});
 
 app.use(bodyParser.urlencoded({extended: true}));
